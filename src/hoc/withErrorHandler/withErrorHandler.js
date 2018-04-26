9d211b962eef7981e2279f6d8c1822bed2d1c3a0
Wojciech Dudek <wdudek82@gmail.com>
2018-04-26 15:00:41 +0200

    Added components for Checkout and CheckoutSummary

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -5,20 +5,25 @@ import Aux from '../Aux';
 const withErrorHandler = (WrappedComponent, axios) => {
   return class extends React.Component {
     state = {
-      error: null
+      error: null,
     };
 
     UNSAFE_componentWillMount() {
-      axios.interceptors.request.use((req) => {
+      this.reqInterceptor = axios.interceptors.request.use((req) => {
         this.setState(() => ({ error: null }));
         return req;
       });
-      axios.interceptors.response.use(
+      this.resInterceptor = axios.interceptors.response.use(
         (res) => res,
         (error) => {
           this.setState(() => ({ error: error }));
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.reqInterceptor);
+      axios.interceptors.request.eject(this.resInterceptor);
     }
 
     errorConfirmedHandler = () => {
