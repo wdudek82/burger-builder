ca022be60bbd11ccc4adb9f41ebbe08aeea52f60
Wojtek Dudek <wdudek82@gmail.com>
2018-05-04 16:05:34 +0200

    Added redux dev tools middleware, and redux-thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import burgerReducer from './store/reducers/burgerReducer'
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const store = createStore(burgerReducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  burgerReducer,
+  composeEnhancers(
+    applyMiddleware(
+      thunk
+    ),
+  )
+);
 
 const app = (
   <Provider store={store}>
